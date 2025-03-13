2a3893adabf70ed1b8a923a9630c3d8074c72351
DESKTOP-RVHIVEJ\Administrator <incregeni@gmail.com>
2025-03-13 13:42:09 -0400

    feat: added contract abis and contract addresses(just minimal version)

---
+++ src/config/abis/index.ts
@@ -0,0 +1,5 @@
+export * from './erc20ABI';
+export * from './factoryABI';
+export * from './pairABI';
+export * from './routerABI';
+export * from './tokenABI';
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+import * as testnet from './contractsTestnet';
+
+export const CONTRACTS = testnet;       // Now, it's for ethereum mainnet
